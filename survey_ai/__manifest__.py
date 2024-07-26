a3c6e9f45bac0e534b2c4dbd3de6c8c5d74df163
Vikas Goyal <info.vikasgoyal@gmail.com>
2024-07-26 15:31:00 +0530

    add details in manifest

--- survey_ai/__manifest__.py
+++ survey_ai/__manifest__.py
@@ -3,6 +3,10 @@
     "summary": "Generate Questions Using AI (chatGPT)",
     "version": "16.0.1",
     "license": "LGPL-3",
+    "author": "Vikas Goyal",
+    "email": "info.vikasgoyal@gmail.com",
+    "company": "",
+    "website": "",
     "category": "survey",
     "depends": ["survey", "chatgpt_base"],
     "data": [
